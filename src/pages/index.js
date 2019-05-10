801116c4fe3d773716c7a0469d6d8d29b1358483
Rohan Uddin <muhammadrohanuddin@gmail.com>
2019-05-09 21:47:51 -0400

    Added Basic CSS and layout modifications

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Learn to design and code React apps</h1>
-    <p>Cillum duis nisi do Lorem cillum magna proident nostrud cillum.</p>
-    
-    
-    <Link to="/page-2/">Go to page two</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+      <h1>Learn to design and code React apps</h1>
+      <p>Cillum duis nisi do Lorem cillum magna proident nostrud cillum.</p>
+      <Link to="/page-2/">Watch the Video</Link>
+      </div>
+    </div>
   </Layout>
 )
 
